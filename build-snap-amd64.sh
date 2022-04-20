01f20619e80780b6a412a9983b87641623f6a377
Alex Fouraker <alex.fouraker@boschrexroth-us.com>
2022-04-20 16:02:45 -0400

    Working Sample

--- build-snap-amd64.sh
+++ build-snap-amd64.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 snapcraft clean --destructive-mode
-snapcraft --destructive-mode --target-arch=amd64 --enable-experimental-target-arch
+snapcraft --destructive-mode --target-arch=amd64 --enable-experimental-target-arch 
